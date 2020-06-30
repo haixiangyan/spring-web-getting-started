e657f668b33a454c9bbbf738079eb883c0ad389e
Haixiang <haixiang6123@gmail.com>
2020-06-30 09:59:11 +0800

    返回 JSON

--- src/main/java/monster/HelloController.java
+++ src/main/java/monster/HelloController.java
@@ -4,13 +4,27 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class HelloController {
 
     @RequestMapping("/search")
-    public String index(@RequestParam("q") String keyword,
-                        @RequestParam(value = "charset", required = false) String charset) {
+    public String search(@RequestParam("q") String keyword,
+                         @RequestParam(value = "charset", required = false) String charset) {
         return "You are searching " + keyword + ", " + charset;
+    }
+
+    @RequestMapping("/search2")
+    public Object search2(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Map<String, Object> map = new HashMap<>();
+        map.put("result", Arrays.asList("aaa", "bbb", "ccc"));
+        return map;
     }
 
 }
