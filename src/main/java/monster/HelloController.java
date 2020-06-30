2a92ed1efabbb8222beb2f5302f0760d73e9288a
Haixiang <haixiang6123@gmail.com>
2020-06-30 15:26:45 +0800

    添加模板引擎

--- src/main/java/monster/HelloController.java
+++ src/main/java/monster/HelloController.java
@@ -1,18 +1,28 @@
 package monster;
 
-import monster.dao.UserMapper;
+import monster.entity.RankItem;
 import monster.service.RankService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class HelloController {
     @Autowired
     private RankService rankService;
 
-    @RequestMapping("/search")
-    public Object search() {
-        return rankService.getRank();
+    @RequestMapping("/")
+    public ModelAndView index() {
+        List<RankItem> rankItems = rankService.getRank();
+
+        Map<String, Object> model = new HashMap<>();
+        model.put("rankItems", rankItems);
+
+        return new ModelAndView("index", model);
     }
 }
